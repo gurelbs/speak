d35e7dde1181b81f94088406bc955679657f1758
gurelbs <guri240@gmail.com>
2021-08-05 17:00:32 +0300

    update

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ function App() {
           e.preventDefault()
           return false;
         }}
+        onClick={startListening}
         onTouchStart={startListening}
         onMouseDown={startListening}
         onTouchEnd={SpeechRecognition.stopListening}
