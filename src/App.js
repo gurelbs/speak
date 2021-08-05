6339d1294ae429dd6c7c6ac98716443fe6b65098
gurelbs <guri240@gmail.com>
2021-08-05 16:57:36 +0300

    update

--- src/App.js
+++ src/App.js
@@ -8,27 +8,37 @@ function App() {
   const {
     transcript,
     finalTranscript,
-    listening,
+    listening,  
     resetTranscript,
     browserSupportsSpeechRecognition
   } = useSpeechRecognition();
   React.useEffect(() => {
     if (finalTranscript){
-      const speak = new SpeechSynthesisUtterance(transcript)
+      const speak = new SpeechSynthesisUtterance(finalTranscript)
       speechSynthesis.speak(speak)
+      console.log(finalTranscript);
     }
   },[finalTranscript])
-  const startListening = () => SpeechRecognition.startListening({ continuous: true });
+  const startListening = () => {
+    resetTranscript()
+    SpeechRecognition.startListening({ continuous: true });
+  }
 
   if (!browserSupportsSpeechRecognition) {
+    console.log(`Browser doesn't support speech recognition.`);
     return <span>Browser doesn't support speech recognition.</span>;
   }
+  console.log(`Browser support speech recognition.`);
 
 
   return (
     <div>
       <p>Microphone: {listening ? 'on' : 'off'}</p>
       <button
+        onCopy={(e)=>{
+          e.preventDefault()
+          return false;
+        }}
         onTouchStart={startListening}
         onMouseDown={startListening}
         onTouchEnd={SpeechRecognition.stopListening}
